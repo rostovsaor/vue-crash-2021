7b37f3d0433a9707cbc077536cc60f041e4aa4d2
Salvador Anselmo Rombe <rostovsaor@gmail.com>
2021-10-21 22:07:31 +0200

    chore: make add task button more generic for reuse

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,10 +2,9 @@
   <header>
     <h1>{{ title }}</h1>
     <Button
-      @toggle-add-task="$emit('toggle-add-task')"
+      @btn-click="$emit('toggle-add-task')"
       :text=" showAddTask ? 'Close' : 'Add Task' "
       :color=" showAddTask ? 'red' : 'green'"
-
     />
   </header>
 </template>
